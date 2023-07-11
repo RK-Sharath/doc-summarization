16d746dea664db1f0d5ad55cc8639f888a3d2f8c
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-11 15:32:28 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().system('pip3 install streamlit langchain openai tiktoken')
+
+
+# In[ ]:
+
+
+import streamlit as st
+from langchain import OpenAI
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+
+
+# In[ ]:
+
+
+openai_api_key="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"
+
+
+# In[ ]:
+
+
+def generate_response(txt):
+    # Instantiate the LLM model
+    llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+    # Split text
+    text_splitter = CharacterTextSplitter()
+    texts = text_splitter.split_text(txt)
+    # Create multiple documents
+    docs = [Document(page_content=t) for t in texts]
+    # Text summarization
+    chain = load_summarize_chain(llm, chain_type='map_reduce')
+    return chain.run(docs)
+
+
+# In[ ]:
+
+
+# Page title
+st.set_page_config(page_title='🦜🔗 Document Summarization App')
+st.title('🦜🔗 Document Summarization App')
+
+
+# In[ ]:
+
+
+# Text input
+txt_input = st.text_area('Enter your text', '', height=200)
+
+# Form to accept user's text input for summarization
+result = []
+with st.form('summarize_form', clear_on_submit=True):
+    openai_api_key = st.text_input('OpenAI API Key', type = 'password', disabled=not txt_input)
+    submitted = st.form_submit_button('Submit')
+    if submitted and openai_api_key.startswith('sk-'):
+        with st.spinner('Calculating...'):
+            response = generate_response(txt_input)
+            result.append(response)
+            del openai_api_key
+
+if len(result):
+    st.info(response)
+
+
+# In[ ]:
+
+
+
+
