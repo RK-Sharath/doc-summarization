7b9b94623fc9970dd8df047dcb1e64a94315535e
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-13 19:21:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,17 +6,23 @@ from langchain.chains.summarize import load_summarize_chain
 
 
 
-def generate_response(txt):
-    # Instantiate the LLM model
-    llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+def generate_res(doc):
+    #Define llm
+    llm = LangChainInterface(
+        model=ModelType.FLAN_T5_11B,
+        credentials=Credentials(api_key=genai_api_key),
+        params=GenerateParams(
+            decoding_method="greedy",
+            max_new_tokens=600,
+            min_new_tokens=150,
+            repetition_penalty=2,
+        ).dict())
     # Split text
-    text_splitter = CharacterTextSplitter()
-    texts = text_splitter.split_text(txt)
-    # Create multiple documents
-    docs = [Document(page_content=t) for t in texts]
+    splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+    chunked_docs = splitter.split_documents(doc)
     # Text summarization
     chain = load_summarize_chain(llm, chain_type='map_reduce')
-    return chain.run(docs)
+    return chain.run(chunked_docs)
 
 
 # Page title
@@ -24,19 +30,19 @@ st.set_page_config(page_title='🦜🔗 Document Summarization App')
 st.title('🦜🔗 Document Summarization App')
 
 
-# Text input
-txt_input = st.text_area('Enter your text', '', height=200)
+# File input
+doc_input = st.file_uploader('Choose your .pdf file', type="pdf")
 
 # Form to accept user's text input for summarization
 result = []
 with st.form('summarize_form', clear_on_submit=True):
-    openai_api_key = st.text_input('OpenAI API Key', type = 'password', disabled=not txt_input)
+    genai_api_key = st.text_input('GenAI API Key', type = 'password', disabled=not doc_input)
     submitted = st.form_submit_button('Submit')
-    if submitted and openai_api_key.startswith('sk-'):
-        with st.spinner('Calculating...'):
-            response = generate_response(txt_input)
+    if submitted and genai_api_key.startswith('pak-'):
+        with st.spinner('Working on it...'):
+            response = generate_res(doc_input)
             result.append(response)
-            del openai_api_key
+            del genai_api_key
 
 if len(result):
     st.info(response)
