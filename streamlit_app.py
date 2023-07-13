4ad80031e1786334a2342858314de1be3f68c5e9
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-13 19:28:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ from langchain import OpenAI, PromptTemplate, LLMChain
 def generate_res(doc):
     #Define llm
     llm = LangChainInterface(
-        model=ModelType.FLAN_T5_11B,
+        model=google/flan-t5-xxl,
         credentials=Credentials(api_key=genai_api_key),
         params=GenerateParams(
             decoding_method="greedy",
