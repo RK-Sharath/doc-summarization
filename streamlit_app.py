e7677563d3e579923c16cee049afe708cfb7116b
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-13 20:19:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,8 +49,9 @@ with st.form('summarize_form', clear_on_submit=True):
         with st.spinner('Working on it...'):
             response = generate_res(doc_input)
             result.append(response)
+            st.info(response)
             del genai_api_key
 
 #if len(result):
-st.info(response)
+#st.info(response)
     
