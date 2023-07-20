c42c4efb14a88e707c30bc6eb5a903ab6e47a87e
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-21 00:35:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,6 @@ uploaded_file = st.file_uploader("Add text file !")
 if uploaded_file:
     for line in uploaded_file:
         st.write(line)
-        line.decode("UTF-8") 
+        str(line, "UTF-8") 
 
     
