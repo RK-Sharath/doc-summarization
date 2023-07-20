1c7c03b2bf3f78f3605a60c231d43612978d4d8f
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-21 01:01:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,9 @@ from io import StringIO
 st.set_page_config(page_title='🦜🔗 Document Summarization App')
 st.title('🦜🔗 Ask questions about the document')
 
+genai_api_key = st.sidebar.text_input("GenAI API Key", type="password")
+genai_api_url = st.sidebar.text_input("GenAI API URL", type="default")
+
 # File input
 
 uploaded_file = st.file_uploader("Add text file !")
@@ -31,6 +34,26 @@ if uploaded_file is not None:
 text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=10)
 chunked_docs = text_splitter.split_text(string_data)
 docs = [Document(page_content=t) for t in chunked_docs]
-st.write(len(docs))
+#st.write(len(docs))
 
+llm = LangChainInterface(model="google/flan-t5-xxl",
+                         credentials=Credentials(api_key=genai_api_key),
+                         params=GenerateParams(
+                             decoding_method="greedy",
+                             max_new_tokens=max_new_tokens,
+                             min_new_tokens=min_new_tokens,
+                             repetition_penalty=2,
+                         ).dict()
+                        ) 
+
+def generate_response(query):
+    chain = load_summarize_chain(llm, chain_type='map_reduce')
+    return chain
+
+with st.form('summarize_form', clear_on_submit=True):
+    submitted = st.form_submit_button('Submit')
+    if submitted and genai_api_key.startswith('pak-'):
+        with st.spinner('Working on it...'):
+            response = generate_response(input_data)
+            st.write(response)
     
