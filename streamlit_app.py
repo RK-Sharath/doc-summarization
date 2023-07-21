564097a0322dc66c903c3565c852e7790ed763c8
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-21 21:50:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,193 @@
-import streamlit as st
-from langchain.docstore.document import Document
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.chains.summarize import load_summarize_chain
-from langchain.document_loaders import UnstructuredPDFLoader, OnlinePDFLoader, PyPDFLoader
 from genai.extensions.langchain import LangChainInterface
 from genai.schemas import ModelType, GenerateParams
 from genai.model import Credentials
-from langchain.chains.mapreduce import MapReduceChain
-from langchain.prompts import PromptTemplate
-from langchain.chains.summarize import load_summarize_chain
-from langchain import OpenAI, PromptTemplate, LLMChain
+import os
+import PyPDF2
+import random
+import itertools
+import streamlit as st
 from io import StringIO
-from langchain import OpenAI
-from langchain.document_loaders import TextLoader
+from langchain.chains import RetrievalQA
+from langchain.retrievers import SVMRetriever
+from langchain.chains import QAGenerationChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.callbacks.base import CallbackManager
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.vectorstores import Chroma
 
 
 # Page title
-st.set_page_config(page_title='🦜🔗 Document Summarization App')
-st.title('🦜🔗 Summarize the document')
+st.set_page_config(page_title='🔗 Retriever Augmented Generation Demo')
+st.caption(This demo was prepared by Sharath Kumar RK, Senior Data Scientist, Watsonx team)
+st.title('🦜🔗 Ask questions about your document')
 
 genai_api_key = st.sidebar.text_input("GenAI API Key", type="password")
 genai_api_url = st.sidebar.text_input("GenAI API URL", type="default")
 max_new_tokens = st.sidebar.text_input("Select max new tokens", type="default")
 min_new_tokens = st.sidebar.text_input("Select min new tokens", type="default")
 
-# File input
+@st.cache_data
+def load_docs(files):
+    st.info("`Reading doc ...`")
+    all_text = ""
+    for file_path in files:
+        file_extension = os.path.splitext(file_path.name)[1]
+        if file_extension == ".pdf":
+            pdf_reader = PyPDF2.PdfReader(file_path)
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+            all_text += text
+        elif file_extension == ".txt":
+            stringio = StringIO(file_path.getvalue().decode("utf-8"))
+            text = stringio.read()
+            all_text += text
+        else:
+            st.warning('Please provide txt or pdf file.', icon="⚠️")
+    return all_text
 
-uploaded_file = st.file_uploader("Add text file !")
-loader = TextLoader(uploaded_file)
-loaded_docs=loader.load()
-st.write(loaded_docs)
+@st.cache_resource
+def split_texts(text, chunk_size, overlap, split_method):
 
-splitter = CharacterTextSplitter(chunk_size=1500, chunk_overlap=10)
-chunked_docs = splitter.split_documents(loaded_docs)
-st.write(len(chunked_docs))
+    st.info("`Splitting doc ...`")
+
+    split_method = "RecursiveTextSplitter"
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=overlap)
+
+    splits = text_splitter.split_text(text)
+    if not splits:
+        st.error("Failed to split document")
+        st.stop()
+
+    return splits
          
+
+@st.cache_resource
+def create_retriever(_embeddings, splits):
+         vectorstore = Chroma.from_texts(splits, _embeddings)
+         retriever = vectorstore.as_retriever
+         return retriever
+
+
+def main():
+    
+    foot = f"""
+    <div style="
+        position: fixed;
+        bottom: 0;
+        left: 30%;
+        right: 0;
+        width: 50%;
+        padding: 0px 0px;
+        text-align: center;
+    ">
+        <p>Made by <a href='https://twitter.com/mehmet_ba7'>Mehmet Balioglu</a></p>
+    </div>
+    """
+
+    st.markdown(foot, unsafe_allow_html=True)
+    
+    # Add custom CSS
+    st.markdown(
+        """
+        <style>
+        
+        #MainMenu {visibility: hidden;
+        # }
+            footer {visibility: hidden;
+            }
+            .css-card {
+                border-radius: 0px;
+                padding: 30px 10px 10px 10px;
+                background-color: #f8f9fa;
+                box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+                margin-bottom: 10px;
+                font-family: "IBM Plex Sans", sans-serif;
+            }
+            
+            .card-tag {
+                border-radius: 0px;
+                padding: 1px 5px 1px 5px;
+                margin-bottom: 10px;
+                position: absolute;
+                left: 0px;
+                top: 0px;
+                font-size: 0.6rem;
+                font-family: "IBM Plex Sans", sans-serif;
+                color: white;
+                background-color: green;
+                }
+                
+            .css-zt5igj {left:0;
+            }
+            
+            span.css-10trblm {margin-left:0;
+            }
+            
+            div.css-1kyxreq {margin-top: -40px;
+            }
+            
+           
+       
+            
+          
+
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    st.sidebar.image("img/logo1.png")
+
+# Use RecursiveCharacterTextSplitter as the default and only text splitter
+    splitter_type = "RecursiveCharacterTextSplitter"
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large",
+                                               model_kwargs={"device": "cpu"}
+                                              )
+
+    if 'genai_api_key' not in st.session_state:
+        genai_api_key = st.text_input(
+            'Please enter your GenAI API key or [get one here](https://platform.openai.com/account/api-keys)', value="", placeholder="Enter the GenAI API key which begins with pak-")
+        if genai_api_key:
+            st.session_state.openai_api_key = genai_api_key
+            os.environ["GENAI_API_KEY"] = genai_api_key
+        else:
+            return
+    else:
+        os.environ["GENAI_API_KEY"] = st.session_state.genai_api_key
+
+    uploaded_files = st.file_uploader("Upload a PDF or TXT Document", type=[
+                                      "pdf", "txt"], accept_multiple_files=True)
+
+    if uploaded_files:
+        # Check if last_uploaded_files is not in session_state or if uploaded_files are different from last_uploaded_files
+        if 'last_uploaded_files' not in st.session_state or st.session_state.last_uploaded_files != uploaded_files:
+            st.session_state.last_uploaded_files = uploaded_files
+                 # Load and process the uploaded PDF or TXT files.
+        loaded_text = load_docs(uploaded_files)
+        st.write("Documents uploaded and processed.")
+
+        # Split the document into chunks
+        splits = split_texts(loaded_text, chunk_size=1000,
+                             overlap=0, split_method=splitter_type)
+
+        # Display the number of text chunks
+        num_chunks = len(splits)
+        st.write(f"Number of text chunks: {num_chunks}")
+
+    retriever = create_retriever(embeddings, splits, retriever_type)
+    qa = RetrievalQA.from_chain_type(llm=chat_openai, retriever=retriever, chain_type="stuff", verbose=True)
+    st.write("Ready to answer questions.")
+
+        # Question and answering
+        user_question = st.text_input("Enter your question:")
+        if user_question:
+            answer = qa.run(user_question)
+            st.write("Answer:", answer)
+
+
+if __name__ == "__main__":
+    main()
     
