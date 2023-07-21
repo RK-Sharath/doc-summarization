7913d44dc15ba304bbfde9696629dca621ca5290
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-21 22:49:38 +0530

    Update doc-summarize.py

--- doc-summarize.py
+++ doc-summarize.py
@@ -48,6 +48,13 @@ def load_docs(files):
             st.warning('Please provide txt or pdf file.', icon="⚠️")
     return all_text
          
+    
+@st.cache_resource
+def create_retriever(_embeddings, splits):
+    vectorstore = Chroma.from_texts(splits, _embeddings)
+    retriever = vectorstore.as_retriever
+    return retriever
+
 @st.cache_resource
 def split_texts(text, chunk_size, overlap, split_method):
 
@@ -63,13 +70,6 @@ def split_texts(text, chunk_size, overlap, split_method):
         st.stop()
 
     return splits
-    
-@st.cache_resource
-def create_retriever(_embeddings, splits):
-         vectorstore = Chroma.from_texts(splits, _embeddings)
-         retriever = vectorstore.as_retriever
-         return retriever
-
 
 
 
