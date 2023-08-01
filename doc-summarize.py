3aa9725714f28a2d4fe28c8f3a9f2d99d1328b04
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-08-01 23:33:39 +0530

    Update doc-summarize.py

--- doc-summarize.py
+++ doc-summarize.py
@@ -19,7 +19,7 @@ st.title("Document Summarization App powered by IBM Watsonx")
 st.caption("This app was developed by Sharath Kumar RK, IBM Ecosystem Engineering Watsonx team")
 
 model = st.radio("Select the Watsonx LLM model",('google/flan-t5-xl','google/flan-t5-xxl','google/flan-ul2'))
-genai_api_key = st.sidebar.text_input("GenAI API Key", type="password")
+#genai_api_key = st.sidebar.text_input("GenAI API Key", type="password")
 genai_api_url = st.sidebar.text_input("GenAI API URL", type="default")
 max_new_tokens = st.sidebar.number_input("Select max new tokens", value=600)
 min_new_tokens = st.sidebar.number_input("Select min new tokens", value=150)
