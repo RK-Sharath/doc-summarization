f32f1a07f6026e3d713ffdb85b7611b83924544d
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-08-03 00:50:40 +0530

    Update doc-summarize.py

--- doc-summarize.py
+++ doc-summarize.py
@@ -111,7 +111,8 @@ def main():
 
          # Load and process the uploaded PDF or TXT files.
         text = load_docs(uploaded_files)
-        st.write(f"Number of text chunks: {text}")
+        total_text = len(text)
+        st.write(f"Number of text chunks: {total_text}")
         st.write("Documents uploaded and processed.")
         # Split the document into chunks
         docs = setup_documents(text, chunk_size, chunk_overlap)
